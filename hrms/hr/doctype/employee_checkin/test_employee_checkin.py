53a6fef0c0495d074b444c63b4fbb88eab70cf2b
laibamasoodd <laiba.masood@bitsol.tech>
2025-02-10 12:40:24 +0000

    fix: python test cases

--- hrms/hr/doctype/employee_checkin/test_employee_checkin.py
+++ hrms/hr/doctype/employee_checkin/test_employee_checkin.py
@@ -123,7 +123,7 @@ class TestEmployeeCheckin(FrappeTestCase):
 		)
 
 		if checkins:
-			attendance_date = getdate(checkins[0].time)
+			attendance_date = str(checkins[0].time).split(" ")[0]
 			attendance = frappe.get_doc(
 				{
 					"doctype": "Attendance",
