43c4f9837f4cce6b419692a527c3c7f9fd5f0e87
laibamasoodd <laiba.masood@bitsol.tech>
2025-03-27 09:47:20 +0000

    leave status logic moved prior to other checks

--- hrms/hr/doctype/employee_checkin/employee_checkin.py
+++ hrms/hr/doctype/employee_checkin/employee_checkin.py
@@ -976,12 +976,14 @@ def check_non_compliance(emp_email, emp_data, api_key, start_dt, end_dt):
 	leave_status = get_employee_leave_status(emp_data["employee"], start_dt.date())
 	min_seconds = HALF_DAY_SECONDS if leave_status == "Half Day" else FULL_DAY_SECONDS
 	half_day_message = " (Half Day)" if min_seconds == HALF_DAY_SECONDS else ""
+	
 	# Compliance Checks
+	if leave_status == "On Leave":
+		return None, None, None  # No non-compliance when on leave
+	
 	if not checkin_time:
 		if total_logged_seconds > 0:
 			return None, None, f"No check-in recorded. {hours} hr {minutes} mins logged{half_day_message}"
-		elif leave_status == "On Leave":
-			return None, None, None  # No non-compliance when on leave
 		else:
 			return None, None, "No check-in, No Clockify logs, No leave recorded"
 
