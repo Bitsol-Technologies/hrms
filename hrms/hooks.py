ace8181ec4ebf38a302d5548b9c9cccca73150a0
laibamasoodd <laiba.masood@bitsol.tech>
2025-04-18 06:25:56 +0000

    indent fix

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -250,7 +250,7 @@ scheduler_events = {
 		"*/15 * * * *": ["hrms.hr.doctype.shift_type.shift_type.notify_employees_to_checkin_or_checkout"],
 		"0 10 * * *": ["hrms.hr.doctype.work_from_home.work_from_home.send_wfh_feedback_forms"],
 		"0 23 * * *": ["hrms.hr.doctype.employee_checkin.employee_checkin.send_daily_compliance_report"],
-        "0 9 * * *": ["hrms.hr.doctype.employee_checkin.employee_checkin.send_yesterday_compliance_report_to_slack"],
+		"0 9 * * *": ["hrms.hr.doctype.employee_checkin.employee_checkin.send_yesterday_compliance_report_to_slack"],
 	},
 }
 
