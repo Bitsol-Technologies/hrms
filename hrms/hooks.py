c16721cbb60c299cf711e3683d0229f2853b8ad8
laibamasoodd <laiba.masood@bitsol.tech>
2025-05-16 10:54:56 +0000

    weekly employee report for hr

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -255,6 +255,7 @@ scheduler_events = {
 		"0 9 * * 1": ["hrms.hr.doctype.employee_checkin.employee_checkin.send_leave_report_to_HR"],
 		"0 9 * * 1": ["hrms.hr.doctype.employee_checkin.employee_checkin.send_wfh_report_to_HR"],
 		"0 9 * * 1": ["hrms.hr.doctype.employee_checkin.employee_checkin.send_weekly_time_report"],
+        "0 9 * * 1": ["hrms.hr.doctype.employee_checkin.employee_checkin.send_weekly_compliance_report_to_HR"],
 	},
 }
 
