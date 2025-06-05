ec551b7ab5d49647df7725995287a61b7721dd3b
laibamasoodd <laiba.masood@bitsol.tech>
2025-06-05 12:37:01 +0000

    logs for checkin checkout

--- hrms/hr/doctype/shift_type/shift_type.py
+++ hrms/hr/doctype/shift_type/shift_type.py
@@ -510,18 +510,5 @@ def notify_employees_to_checkin_or_checkout():
 				title="Time to Check Out!",
 				message="Your shift is almost over. Please remember to check out. Have a great evening!",
 			)
-			log_entry = frappe.new_doc("Notification Log")
-			log_entry.document_type = "Employee"
-			log_entry.document_name = employee.name
-			log_entry.subject = f"Checkout Reminder Notifications"
-			log_entry.email_content = f"Employees notified for Check Out: {notify_checkout}"
-			log_entry.type = "Alert"
-			log_entry.flags.ignore_permissions = True
-			log_entry.insert() # Insert the document
-			frappe.db.set_value("Notification Log", log_entry.name, {
-				"for_user": employee.user_id, 
-				"read": 1
-			})
-			frappe.db.commit()
 		notification_logger.info(f"Employees to be notified for Check In: {notify_checkin}")
 		notification_logger.info(f"Employees to be notified for Check Out: {notify_checkout}")
