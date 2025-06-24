bd549eda76230512f5b4c7cf3e650e34f6ef0ce5
laibamasoodd <laiba.masood@bitsol.tech>
2025-06-24 14:37:00 +0500

    email templates defined in doctype (#97)

--- hrms/hr/doctype/hr_notifications/hr_notifications.js
+++ hrms/hr/doctype/hr_notifications/hr_notifications.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("HR Notifications", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("HR Notifications", {
+	type(frm) {
+        if (frm.is_new()) {
+			if (frm.doc.type && frm.doc.type !== "Other") {
+				frappe.db.get_doc("Email Template", frm.doc.type).then((template) => {
+					if (template) {
+						frm.set_value("subject", template.subject);
+						frm.set_value("message", template.response_html);
+					}
+				});
+			} else {
+				frm.set_value("subject", "");
+				frm.set_value("message", "");
+			}
+		}
+	},
+});
