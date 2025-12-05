270bb2c492ab873677760c24340f92a548e3a0a8
Muhammad Aamir Zaman <aamir.zaman@bitsol.tech>
2025-12-05 10:27:53 +0000

    notification log added for rejected email

--- hrms/hr/doctype/interview/interview.py
+++ hrms/hr/doctype/interview/interview.py
@@ -5,7 +5,7 @@ import requests
 import uuid
 from datetime import datetime, timedelta
 import json
-# from google.apps import meet_v2
+from google.apps import meet_v2
 from google.oauth2.service_account import Credentials
 
 import frappe
